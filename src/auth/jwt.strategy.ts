0773b636b008b0129d25a051adf60a3bc71bc9b8
nahmanbo <nhmanbo2003@gmail.com>
2025-08-03 15:49:49 +0300

    implement JWT strategy for validating token payload

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/users/user.entity';
+
+@Injectable()
+// Handles JWT token validation and attaches payload to req.user
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'dev-secret',
+    });
+  }
+
+  // Returns decoded payload (becomes req.user)
+  async validate(payload: Partial<User>) {
+    return payload;
+  }
+}
