391a58f2ad44909e4673cbcc5dbb8ded3dc29415
nahmanbo <nhmanbo2003@gmail.com>
2025-08-03 15:57:06 +0300

    cleanup(users.controller): remove login endpoint from users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -11,11 +10,5 @@ export class UsersController {
   @Post('signup')
   signup(@Body() body: CreateUserDto) {
     return this.usersService.signup(body);
-  }
-
-  // Logs in a user
-  @Post('login')
-  login(@Body() body: LoginUserDto) {
-    return this.usersService.login(body);
   }
 }
